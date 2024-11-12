39aec762d8cd04e827e971472be346708753fa25
nicolasm1605 <nicolas.moreno@utp.edu.co>
2024-11-12 10:14:53 -0500

    vercel0.2

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; // Cambiado a 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css'; // Asegúrate de que esto está presente para importar Tailwind CSS
+import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Consumerism"> {/* Aquí está el BrowserRouter */}
+    <BrowserRouter basename="/"> {/* Aquí está el BrowserRouter */}
       <App />
     </BrowserRouter>
   </React.StrictMode>

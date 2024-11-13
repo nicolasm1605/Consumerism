bd5c6f162a376eb8d38a49a3e5f556facd1c105a
nicolasm1605 <nicolas.moreno@utp.edu.co>
2024-11-13 11:45:44 -0500

    diseño responsive 1.1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,38 +7,40 @@ const Header = () => {
 
   return (
     <div className="bg-gray-800">
-      <div className="w-auto pl-10 pr-10 m-auto  flex justify-between items-center py-5 ">
-        {/* Título */}
+      <div className="w-4/5 mx-auto flex justify-between items-center py-5 gap-x-4">
+        {/* Título a la izquierda */}
         <Link to="/">
           <motion.h1 
             whileHover={{ scale: 1.2 }} 
             whileTap={{ scale: 0.95 }} 
-            className="text-2xl w-2/3 bg-green-200 px-4 py-1 text-center rounded-lg font-bold "
+            className="text-3xl w-auto bg-green-200 px-2 py-1 rounded-lg font-bold"
           >
             Conscious Consumerism
           </motion.h1>
         </Link>
 
-        {/* Botón de menú */}
-        <button 
-          className="text-black font-bold px-4 py-2 rounded-md bg-green-200"
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          Menu
-        </button>
+        {/* Botón de menú a la derecha */}
+        <div className="flex items-center">
+          <button 
+            className="text-black font-bold px-4 py-2 rounded-md bg-green-200"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            Menu
+          </button>
 
-        {/* Menú desplegable */}
-        {menuOpen && (
-          <div className="absolute right-5 top-12 bg-gray-700 p-4 rounded-lg shadow-lg">
-            <ul className="space-y-2">
-              <li><Link to="/post1" className="text-white hover:text-green-300">Post 1</Link></li>
-              <li><Link to="/post2" className="text-white hover:text-green-300">Post 2</Link></li>
-              <li><Link to="/post3" className="text-white hover:text-green-300">Post 3</Link></li>
-              <li><Link to="/post4" className="text-white hover:text-green-300">Post 4</Link></li>
-              <li><Link to="/post5" className="text-white hover:text-green-300">Post 5</Link></li>
-            </ul>
-          </div>
-        )}
+          {/* Menú desplegable */}
+          {menuOpen && (
+            <div className="absolute right-5 top-16 bg-gray-700 p-4 rounded-lg shadow-lg">
+              <ul className="space-y-2">
+                <li><Link to="/post1" className="text-white hover:text-green-300">Post 1</Link></li>
+                <li><Link to="/post2" className="text-white hover:text-green-300">Post 2</Link></li>
+                <li><Link to="/post3" className="text-white hover:text-green-300">Post 3</Link></li>
+                <li><Link to="/post4" className="text-white hover:text-green-300">Post 4</Link></li>
+                <li><Link to="/post5" className="text-white hover:text-green-300">Post 5</Link></li>
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
